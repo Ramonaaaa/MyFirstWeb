6a50b2e6e671e10aef66f4144af209e081c2580f
Ramona Mihai <ramona.mihai87@yahoo.com>
2019-01-09 18:43:52 +0200

    My first web

---
+++ WebFirstHomework/Controllers/DogsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebFirstHomework.Models;
+
+namespace WebFirstHomework.Controllers
+{
+    public class DogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List(FurColor? color, Gender? gender)
+        {
+            var dog = DogsList();
+
+            if (gender.HasValue)
+            {
+                dog = dog.Where(x => x.Gender == gender.Value).ToList();
+            }
+
+            if (color.HasValue)
+            {
+                dog = dog.Where(x => x.FurColor == color.Value).ToList();
+            }
+
+            return View(dog);
+        }
+
+        private List<Dogs> DogsList()
+        {
+            Console.WriteLine("testing the branch.");
+            List<Dogs> dog = new List<Dogs>();
+
+            dog.Add(new Dogs()
+            {
+                DogName = "Lord",
+                DogOwner = "Ramona",
+                FurColor = FurColor.White,
+                Gender = Gender.Male
+            });
+
+            dog.Add(new Dogs()
+            {
+                DogName = "Blacky",
+                DogOwner = "Cecilia",
+                FurColor = FurColor.Black,
+                Gender = Gender.Female
+            });
+
+            dog.Add(new Dogs()
+            {
+                DogName = "Jerky",
+                DogOwner = "Roxana",
+                FurColor = FurColor.Yellow,
+                Gender = Gender.Female
+            });
+
+            dog.Add(new Dogs()
+            {
+                DogName = "Haidi",
+                DogOwner = "Theo",
+                FurColor = FurColor.Black,
+                Gender = Gender.Male
+            });
+
+            dog.Add(new Dogs()
+            {
+                DogName = "Uzu",
+                DogOwner = "Raluca",
+                FurColor = FurColor.White,
+                Gender = Gender.Male
+            });
+
+            return dog;
+        }
+    }
+}
---
+++ WebFirstHomework/Models/Dogs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFirstHomework.Models
+{ 
+    public class Dogs
+    {
+        public string DogName { get; set; }
+        public string DogOwner { get; set; }
+        public Gender Gender { get; set; }
+        public FurColor FurColor { get; set; }
+    }
+}
